56b2a4686e2581910ffa6b6ffa5784910f584fd9
jordihofc <jordi.silva@ufv.br>
2021-04-03 16:26:35 -0300

    refatorando handler

--- src/main/java/br/com/controleestoque/ControleEstoque/handler/HandlerErros.java
+++ src/main/java/br/com/controleestoque/ControleEstoque/handler/HandlerErros.java
@@ -14,7 +14,7 @@ public class HandlerErros {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> methodArgumentNotValid(MethodArgumentNotValidException e){
         List<Erros> erros=e.getBindingResult().getFieldErrors().stream().map(Erros::new).collect(Collectors.toList());
-        erros.addAll(e.getBindingResult().getGlobalErrors().stream().map(err-> new Erros(err.getObjectName(), err.getDefaultMessage())).collect(Collectors.toList()));
+        erros.addAll(e.getBindingResult().getGlobalErrors().stream().map(err-> new Erros(err.getObjectName().substring(5,12).toLowerCase(), err.getDefaultMessage())).collect(Collectors.toList()));
         return ResponseEntity.badRequest().body(erros);
     }
 }
